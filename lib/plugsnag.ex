efdaee9d2abde442b50b54984419450725afad58
Johan Lind <johan.behe.lind@gmail.com>
2015-10-29 21:54:47 +0100

    Add connection meta data

--- lib/plugsnag.ex
+++ lib/plugsnag.ex
@@ -17,7 +17,9 @@ defmodule Plugsnag do
             stacktrace = System.stacktrace
 
             exception
-            |> Bugsnag.report(releaseStage: Atom.to_string(Mix.env))
+            |> Bugsnag.report(releaseStage: Atom.to_string(Mix.env), metaData: ([:host, :method, :path_info, :script_name, :request_path, :port, :scheme, :query_string]
+            |> Enum.reduce(%{}, fn(x, acc) -> Map.put(acc, x, Map.get(conn, x)) end)
+            |> Map.put(:req_headers, conn.req_headers |> Enum.into(%{}))))
 
             reraise exception, stacktrace
         end
